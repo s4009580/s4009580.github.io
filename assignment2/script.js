c8bdbe526c7c67f573a55807fb3f8aebc6fbbf7e
papipin <s4009580@student.rmit.edu.au>
2024-05-19 17:40:16 +1000

    waow

--- assignment2/script.js
+++ assignment2/script.js
@@ -17,15 +17,98 @@ function toggleVideoPlayback()
 {
     if(myVideo.paused || myVideo.ended)
     {
-        playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v2.png";
+        playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v2.png";
         myVideo.play();
     }
     else
     {
-        playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v2.png";
+        playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v2.png";
         myVideo.pause();
     }
 }
+// The following code allows me to mute and unmute a video on click of a button
+
+const muteUnmuteButton = document.querySelector("#mute-unmute-btn");
+console.log(muteUnmuteButton);
+
+muteUnmuteButton.addEventListener("click", toggleSound);
+
+myVideo.addEventListener("click", toggleSound);
+function toggleSound()
+{
+    if(myVideo.muted)
+    {
+
+    muteUnmuteButton.style.backgroundColor = "#ac875e"
+        myVideo.muted = false;
+
+    }
+    else
+    {
+        muteUnmuteButton.style.backgroundColor = "#755837"
+        myVideo.muted = true;
+    }
+}
+
+// ----------------------------------------------------------
+// The following code will show the progress of the video //
+myVideo.addEventListener("timeupdate", updateProgressBar);
+
+const progressBarFill = document.querySelector("#progress-bar-fill");
+console.log(progressBarFill);
+
+function updateProgressBar()
+{
+const progress = (myVideo.currentTime / myVideo.duration) * 100;
+progressBarFill.style.width = progress + "%";
+console.log(progress);
+}
+
+// ----------------------------------------------------------
+// The following code will navigate two timelines of the video //
+
+const step1Button = document.querySelector("#step1");
+console.log(step1Button);
+step1Button.addEventListener("click", gotoStep1);
+
+function gotoStep1()
+{
+    myVideo.currentTime = 16.0;
+}
+
+const step2Button = document.querySelector("#step2");
+console.log(step2Button);
+step2Button.addEventListener("click", gotoStep2);
+
+function gotoStep2()
+{
+    myVideo.currentTime = 43.0;
+}
+
+
+// ----------------------------------------------------------
+// The following code will go full screen mode for the vidoe //
+myVideo.addEventListener("dblclick", goFullscreen);
+
+
+const fullscreenButton = document.querySelector("#fullscreen-btn");
+console.log(fullscreenButton);
+
+fullscreenButton.addEventListener("click", goFullscreen);
+
+function goFullscreen()
+{
+    if(!document.fullscreenElement)
+    {
+        myVideo.requestFullscreen();
+
+    }
+    else
+    {
+        document.exitFullscreen();
+    }
+}
+
 
 // This is a start stop timer code from https://stackoverflow.com/questions/63798545/how-to-stop-timer-count-and-start-with-saved-time //
 
